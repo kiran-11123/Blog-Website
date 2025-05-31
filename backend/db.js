9638180b3addca8517274ab4c777f9313656ae1f
Kiran <kiran.sadara6730@gmail.com>
2025-05-31 13:59:07 +0530

    Clean commit without .env

---
+++ backend/db.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const  ConnectDb = async()=>{
+
+
+    try{
+
+        await mongoose.connect("mongodb://localhost:27017/Blog_app",{
+
+         useNewUrlParser: true,
+         useUnifiedTopology: true
+
+    })
+
+    console.log("Database connected successfully");
+ 
+    }
+
+    catch(e){
+        console.log("Error connecting the database" , e);
+    }
+
+   
+}
+
+
+module.exports = ConnectDb ; 
